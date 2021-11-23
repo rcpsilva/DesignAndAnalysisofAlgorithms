5dadbef77b0bcd4bc5862564b6151b169f37ced0
rcpsilva <rcpsilva@gmail.com>
2021-11-23 14:43:08 -0300

    chore: Knapsack problem. Classroom version.

--- Knapsack.cpp
+++ Knapsack.cpp
@@ -2,30 +2,25 @@
 #include <algorithm>
 #include "Knapsack.h"
 
-
-int mf_knapsack(int i, int j, std::vector<std::vector<int>>& F,
-	std::vector<int>& values, std::vector<int>& weights) {
-	
+int mf_knapsack(int i, int j, std::vector<std::vector<int>> F, std::vector<int>& values, std::vector<int>& weights) {
 
 	if (F[i][j] < 0) {
-		int value = -1;
 		if (j < weights[i-1]) {
-			value = mf_knapsack(i - 1, j, F, values, weights);
+			F[i][j] = mf_knapsack(i - 1, j, F, values, weights);
 		}
-		else{
-			value = std::max(mf_knapsack(i - 1, j, F, values, weights), 
-				values[i-1] + mf_knapsack(i - 1, j - weights[i-1], F, values, weights));
+		else {
+			F[i][j] = std::max(mf_knapsack(i - 1, j, F, values, weights),
+				values[i-1] + mf_knapsack(i-1, j - weights[i-1],F,values, weights));
 		}
-		F[i][j] = value;
+	
 	}
 
 	return F[i][j];
-
 }
 
 int mf_knapsack(std::vector<int>& values, std::vector<int>& weights, int capacity) {
 
-	std::vector<std::vector<int>> F(weights.size()+1, std::vector<int>(capacity+1, -1));
+	std::vector<std::vector<int>> F(weights.size() + 1, std::vector<int>(capacity + 1, -1));
 
 	for (size_t i = 0; i < F.size(); i++)
 	{
@@ -33,10 +28,33 @@ int mf_knapsack(std::vector<int>& values, std::vector<int>& weights, int capacit
 	}
 
 	for (size_t i = 0; i < F[0].size(); i++)
-	{	
+	{
 		F[0][i] = 0;
 	}
 
-	return mf_knapsack(F.size()-1, F[0].size()-1, F, values, weights);
+	return mf_knapsack(F.size() - 1, F[0].size() - 1, F, values, weights);
 
+}
+
+int rec_knapsack(int i, int j, std::vector<int>& values, std::vector<int>& weights) {
+
+	if (i == 0 || j == 0) {
+		return 0;
+	}
+	else {
+
+		if (j < weights[i - 1]) {
+			return rec_knapsack(i - 1, j, values, weights);
+		}
+		else {
+			return std::max(rec_knapsack(i - 1, j, values, weights),
+				values[i - 1] + rec_knapsack(i - 1, j - weights[i - 1], values, weights));
+		}
+	}
+	
+}
+
+int rec_knapsack(std::vector<int>& values, std::vector<int>& weights, int capacity) {
+
+	return rec_knapsack(values.size(), capacity, values, weights);
 }
--- Knapsack.h
+++ Knapsack.h
@@ -1,3 +1,4 @@
 #pragma once
 
 int mf_knapsack(std::vector<int>& values, std::vector<int>& weights, int capacity);
+int rec_knapsack(std::vector<int>& values, std::vector<int>& weights, int capacity);
