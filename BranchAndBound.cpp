9ad945d4755ddd24a0fce70c95eb4d5bd2f9b5e3
rcpsilva <rcpsilva@gmail.com>
2021-11-29 18:36:16 -0300

    update: Branch and bound for the TSP problem. Class version.

--- BranchAndBound.cpp
+++ BranchAndBound.cpp
@@ -9,22 +9,25 @@ int cost(std::vector<std::vector<int>>& graph, std::vector<int>& tour) {
 
 	int cost = 0;
 
-	for (int i = 0; i < tour.size(); i++)
+	for (size_t i = 0; i < tour.size()-1; i++)
 	{
-		cost += graph[tour[i]][tour[(i + 1) % tour.size()]];
+		cost += graph[tour[i]][tour[i + 1]];
+	}
+
+	if (tour.size() == graph.size()) {
+		cost += graph[tour[graph.size() - 1]][tour[0]];
 	}
 
 	return cost;
+}
+
+int heuristic(std::vector<int>& tour, int min_edge, int n_cities) {
+
+	return (n_cities - tour.size() + 1) * min_edge;
 
 }
 
-int heuristic(std::vector<int>& tour, int min_edge, int n) {
-
-	return (n - tour.size() + 1) * min_edge;
-
-}
-
-int is_possible_tsp(std::vector<int>& tour, int city) {
+bool is_possible_tsp(std::vector<int>& tour, int city) {
 
 	for (size_t i = 0; i < tour.size(); i++)
 	{
@@ -34,32 +37,38 @@ int is_possible_tsp(std::vector<int>& tour, int city) {
 	}
 
 	return true;
+
 }
 
-void solve_tsp(std::vector<int>& tour, std::vector<std::vector<int>>& graph, int min_edge, int bound,
-	std::vector<int>& best_tour, int best_cost) {
-
+void solve_tsp(std::vector<int>& tour, std::vector<std::vector<int>>& graph, int min_edge, int& bound,
+	std::vector<int>& best_tour, int& best_cost) 
+{
 	if (tour.size() == graph.size()) {
-		int tour_cost = cost(graph, tour);
+		int tour_cost = cost(graph,tour);
+
 		if (tour_cost < bound) {
-			bound = tour_cost;	
-			if (tour_cost < best_cost) {
-				best_cost = tour_cost;
-				best_tour = tour;
-			}
+			bound = tour_cost;
 		}
+
+		if (tour_cost < best_cost) {
+			best_tour = tour;
+			best_cost = tour_cost;
+		}
+
+
 		printSequence(tour);
 		std::cout << "cost: " << tour_cost << std::endl;
-		std::cout << "==================== " << std::endl;
+		std::cout << "========================" << std::endl;
+
 		return;
 	}
 	else {
-		for (int i = 1; i < graph.size(); i++)
+		for (int i = 0; i < graph.size(); i++)
 		{
 			if (is_possible_tsp(tour, i)) {
-				if ((cost(graph, tour) + heuristic(tour, min_edge, i)) < bound){
+				if (cost(graph,tour) + heuristic(tour, min_edge, i) < bound){
 					tour.push_back(i);
-					solve_tsp(tour, graph, min_edge, bound, best_tour, best_cost);
+					solve_tsp(tour,graph,min_edge, bound, best_tour, best_cost);
 					tour.pop_back();
 				}
 			}
@@ -71,25 +80,28 @@ void solve_tsp(std::vector<int>& tour, std::vector<std::vector<int>>& graph, int
 
 void solve_tsp(std::vector<std::vector<int>>& graph) {
 
-	int minedge = std::numeric_limits<int>::max();
+	int min_edge = std::numeric_limits<int>::max();
 
 	for (size_t i = 0; i < graph.size(); i++)
 	{
 		for (size_t j = 0; j < graph.size(); j++)
 		{
-			minedge = (graph[i][j] < minedge) ? graph[i][j] : minedge;
+			if (graph[i][j] < min_edge) {
+				min_edge = graph[i][j];
+			}
 		}
 	}
 
 	std::vector<int> tour({ 0 });
 	std::vector<int> best_tour({ 0 });
-	static int best_cost = std::numeric_limits<int>::max();
-	
+	int best_cost = std::numeric_limits<int>::max();
+	int bound = 15; // std::numeric_limits<int>::max();
 
-	solve_tsp(tour, graph, minedge, 17, best_tour, best_cost);
+	solve_tsp(tour, graph, min_edge, bound, best_tour, best_cost);
 
-	std::cout << "BEST Tour: " << std::endl;
+	std::cout << "BEST TOUR:" << std::endl;
 	printSequence(best_tour);
-	std::cout << "cost: " << cost(graph,best_tour) << std::endl;
-	std::cout << "==================== " << std::endl;
+	std::cout << "cost: " << cost(graph, best_tour) << std::endl;
+	std::cout << "========================" << std::endl;
+
 }
